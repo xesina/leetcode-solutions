1bf4b632762a13fc1448a792411099d0415de674
Sina Saeidi <xesina@gmail.com>
2019-05-17 01:44:32 +0430

    add solution for two-sum problem

---
+++ two-sum/main.go
@@ -0,0 +1,31 @@
+package main
+
+func main()  {
+	
+}
+
+func twoSum(nums []int, target int) []int {
+    l := len(nums)
+    var result []int
+    m := make(map[int]int, len(nums))
+    for i:=0; i<l; i++ {
+        
+        if v, ok := m[nums[i]]; ok && nums[i]*2==target  {
+           result := append(result, v, i)
+            return result 
+        }
+        
+        m[nums[i]] = i
+        
+        d := target - nums[i]
+        
+        if v, ok := m[d]; ok && i != v {
+            result := append(result, v, i)
+            return result
+        }
+        
+       
+    }
+    
+    return result
+}
