b8e3d650365590b0171bfd4717c21b527a6519f2
Sina Saeidi <xesina@gmail.com>
2019-05-31 01:59:11 +0430

    add another anagram solution with map

--- valid-anagram/valid_anagram.go
+++ valid-anagram/valid_anagram.go
@@ -25,3 +25,33 @@ func isAnagram(s string, t string) bool {
 
 	return true
 }
+
+// To examine if tt is a rearrangement of ss, we can count
+// occurrences of each letter in the two strings and compare them.
+func isAnagram2(s string, t string) bool {
+	s = strings.Join(strings.Fields(s), "")
+	t = strings.Join(strings.Fields(t), "")
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	sm := make(map[rune]int)
+	for _, v := range s {
+		sm[v]++
+	}
+
+	for _, v := range t {
+		if _, ok := sm[v]; ok {
+			sm[v]--
+		}
+	}
+
+	for _, v := range sm {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
+}
